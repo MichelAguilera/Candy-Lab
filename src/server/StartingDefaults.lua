533ccb72a9a7df317c1cab68eae56ee4d35b6ca8
MichelAguilera <48520019+MichelAguilera@users.noreply.github.com>
2022-12-08 21:11:01 +0100

    init

---
+++ src/server/StartingDefaults.lua
@@ -0,0 +1,15 @@
+-- Generic Dependencies
+local sm  = require(game:GetService("ReplicatedStorage"):WaitForChild("Common"):WaitForChild("ServiceManager"))
+
+-- Specific Dependencies
+
+
+-- Starting Defaults
+StartingDefaults = {}
+
+StartingDefaults.RealEstate = {}
+StartingDefaults.Production = {}
+StartingDefaults.Technology = {}
+StartingDefaults.Relations  = {}
+
+return StartingDefaults
